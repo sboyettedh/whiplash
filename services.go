b0f31b5345cd67afd65c9ff62cdb98dade731c71
Shawn Boyette <shawn.boyette@dreamhost.com>
2015-09-12 02:55:09 -0400

    don't allocate byteslices every time we ask ceph something

--- services.go
+++ services.go
@@ -33,6 +33,11 @@ type Svc struct {
 
 	// Version is the Ceph version of the service (if reporting)
 	Version string
+
+	// b1 is the buffer we read into from the network
+	b1 []byte
+	// b2 accumulates data from b1
+	b2 []byte
 }
 
 type cephVersion struct {
@@ -44,33 +49,36 @@ func (wlc *WLConfig) getCephServices() {
 	wlc.Svcs = make(map[string]*Svc)
 	// iterate over CephConf, adding OSDs and RGWs
 	for k, m := range wlc.CephConf {
+		s := &Svc{b1: make([]byte, 64)}
 		switch {
 		case strings.HasPrefix(k, "osd."):
-			s := &Svc{Type: OSD, Host: m["host"]}
+			s.Type = OSD
+			s.Host = m["host"]
 			s.Sock = strings.Replace(wlc.CephConf["osd"]["admin socket"], "$name", k, 1)
-			vbytes, err := svcQuery(s.Sock, "version")
+			err := s.Query("version")
 			if err == nil {
 				s.Reporting = true
 			}
 			vs := &cephVersion{}
-			err = json.Unmarshal(vbytes, vs)
+			err = json.Unmarshal(s.b2, vs)
 			if err == nil {
 				s.Version = vs.version
 			}
 			wlc.Svcs[k] = s
 		case strings.HasPrefix(k, "client.radosgw"):
-			s := &Svc{Type: RGW, Host: os.Getenv("HOSTNAME")}
+			s.Type = RGW
+			s.Host = os.Getenv("HOSTNAME")
 			if rsp, ok := m["rgw socket path"]; ok {
 				s.Sock = rsp
 			} else {
 				s.Sock = strings.Replace(m["admin socket"], "$name", k, 1)
 			}
-			vbytes, err := svcQuery(s.Sock, "version")
+			err := s.Query("version")
 			if err == nil {
 				s.Reporting = true
 			}
 			vs := &cephVersion{}
-			err = json.Unmarshal(vbytes, vs)
+			err = json.Unmarshal(s.b2, vs)
 			if err == nil {
 				s.Version = vs.version
 			}
@@ -80,14 +88,14 @@ func (wlc *WLConfig) getCephServices() {
 	// if we get down here and Svcs is empty, we're on a monitor
 	if len(wlc.Svcs) == 0 {
 		k := "mon." + os.Getenv("HOSTNAME")
-		s := &Svc{Type: MON, Host: wlc.CephConf[k]["host"]}
+		s := &Svc{Type: MON, Host: wlc.CephConf[k]["host"], b1: make([]byte, 64)}
 		s.Sock = strings.Replace(wlc.CephConf["osd"]["admin socket"], "$name", k, 1)
-		vbytes, err := svcQuery(s.Sock, "version")
+		err := s.Query("version")
 		if err == nil {
 			s.Reporting = true
 		}
 		vs := &cephVersion{}
-		err = json.Unmarshal(vbytes, vs)
+		err = json.Unmarshal(s.b2, vs)
 		if err == nil {
 			s.Version = vs.version
 		}
@@ -95,14 +103,11 @@ func (wlc *WLConfig) getCephServices() {
 	}
 }
 
-func svcQuery(sock, cmd string) ([]byte, error) {
-	b1 := make([]byte, 64)
-	var b2 []byte
-
+func (s *Svc) Query(cmd string) error {
 	// make the connection
-	conn, err := net.Dial("unix", sock)
+	conn, err := net.Dial("unix", s.Sock)
 	if err != nil {
-		return nil, fmt.Errorf("could not connect to sock %v: %v\n", sock, err)
+		return fmt.Errorf("could not connect to sock %v: %v\n", s.Sock, err)
 	}
 	defer conn.Close()
 
@@ -110,15 +115,16 @@ func svcQuery(sock, cmd string) ([]byte, error) {
 	conn.SetDeadline(time.Now().Add(250 * time.Millisecond))
 	_, err = conn.Write([]byte(cmd + "\000"))
 	if err != nil {
-		return nil, fmt.Errorf("could not write to %v: %v\n", sock, err)
+		return fmt.Errorf("could not write to %v: %v\n", s.Sock, err)
 	}
 
-	// now read what we got back.
+	// zero our byte-collector and read what we got back.
+	s.b2 = s.b2[:0]
 	conn.SetDeadline(time.Now().Add(50 * time.Millisecond))
 	for {
-		n, err := conn.Read(b1)
+		n, err := conn.Read(s.b1)
 		if err != nil && err.Error() != "EOF" {
-			return nil, fmt.Errorf("could not read from %v: %v\n", sock, err)
+			return fmt.Errorf("could not read from %v: %v\n", s.Sock, err)
 		}
 		// since the admin-daemon closes the connection as soon as
 		// it's done writing, there's no EOM to watch for. you just
@@ -126,7 +132,7 @@ func svcQuery(sock, cmd string) ([]byte, error) {
 		if n == 0 {
 			break
 		}
-		b2 = append(b2, b1[:n]...)
+		s.b2 = append(s.b2, s.b1[:n]...)
 	}
-	return b2, err
+	return err
 }
